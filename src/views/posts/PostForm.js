cd43895649219812542b06b143586f2573d73def
Sean Rose <sean1rose@gmail.com>
2018-07-24 23:38:00 -0700

    list vs rank type, still need to save that way tho

--- src/views/posts/PostForm.js
+++ src/views/posts/PostForm.js
@@ -22,6 +22,14 @@ class PostForm extends React.Component {
 
   state = {
     disabled: false,
+    listType: "rank",
+    rows: [
+      {"rank": "one", "list": "a"},
+      {"rank": "two", "list": "b"},
+      {"rank": "three", "list": "c"},
+      {"rank": "four", "list": "d"},
+      {"rank": "five", "list": "e"},
+    ],
   }
 
   submit = event => {
@@ -52,15 +60,44 @@ class PostForm extends React.Component {
     })
   }
 
+  changeListType = event => {
+    event.preventDefault();
+    if (this.state.listType === "rank") {
+      // change to unordered list
+      this.setState({
+        listType: "list"
+      });
+    } else if (this.state.listType === "list") {
+      this.setState({
+        listType: "rank"
+      });
+    }
+  }
+
   render() {
     return <form onSubmit={this.submit}>
 
+
       <FormRow>
-        <TopLabel for="title">List Name:</TopLabel>
+        {/* <div style={{display: "flex"}}>
+          <Button style={{border: "none"}} onClick={this.changeListType}>{this.state.listType === "rank" ? "Ranking" : "List"}</Button>
+          <TopLabel style={{paddingTop: "1.25%", fontSize: "16px"}} for="title">Name</TopLabel>
+        </div> */}
+        <Button onClick={this.changeListType}>{this.state.listType === "rank" ? "Ranking" : "List"}</Button>
+        <TopLabel for="title">{this.state.listType === "rank" ? "Ranking Name" : "List Name"}</TopLabel>
         <Input type="text" name="title" defaultValue={(this.props.post && this.props.post.title) || ''} required />
       </FormRow>
 
-      <FormRow>
+      {this.state.rows.map((row, idx) => {
+        return (
+          <FormRow key={idx}>
+            <Label for={row.rank}>{this.state.listType === "rank" ? `${idx + 1})` : `${row.list})`}</Label>
+            <Input type="text" name={row.rank} defaultValue={(this.props.post && this.props.post[`${row.rank}`]) || ''} required />
+          </FormRow>
+        )
+      })}
+
+      {/* <FormRow>
         <Label for="one">1) </Label>
         <Input type="text" name="one" defaultValue={(this.props.post && this.props.post.one) || ''} required />
       </FormRow>
@@ -83,7 +120,7 @@ class PostForm extends React.Component {
       <FormRow>
         <Label for="five">5) </Label>
         <Input type="text" name="five" defaultValue={(this.props.post && this.props.post.five) || ''} required />
-      </FormRow>
+      </FormRow> */}
 
 
       <Button type="submit" disabled={this.state.disabled}>Submit</Button>
